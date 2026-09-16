08d2f2d5db1750a42f9996ee22ab26110ef4a619
culturaydesarrollo <corporacion.cultura.y.desarrollo@gmail.com>
2026-09-16 00:01:09 -0500

    chore: actualiza contenido y estilos del blog

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -6,6 +6,7 @@ export default new Map([
 ["src/content/blog/convocatoria2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fconvocatoria2.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/curaduria-img-pat.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fcuraduria-img-pat.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/El patrimonio no se conserva se vive.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FEl+patrimonio+no+se+conserva+se+vive.mdx&astroContentModuleFlag=true")],
+["src/content/blog/mito-del-sereno.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmito-del-sereno.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/primer-post.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fprimer-post.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/proximos_anuncios.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fproximos_anuncios.mdx&astroContentModuleFlag=true")]]);
 		
