dc1f2808d7276f94a5626089bf4e951d5e938114
corporacionculturadesarrollo <corporacionculturadesarrollo@gmail.com>
2026-09-19 11:00:11 -0500

    Add new author and blog post
    This commit adds the Dimitri Guzmán author profile and a new blog post, "Toque de cacho por las trochas de la memoria," along with its cover image. It also updates the generated Astro content registry so the new content is recognized by the site.

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,7 @@
 
 export default new Map([
 ["src/content/autores/corporacion-cultura-y-desarrollo.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fautores%2Fcorporacion-cultura-y-desarrollo.mdx&astroContentModuleFlag=true")],
+["src/content/autores/dimitri-guzman.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fautores%2Fdimitri-guzman.mdx&astroContentModuleFlag=true")],
 ["src/content/autores/hector-galeano-david.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fautores%2Fhector-galeano-david.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/convocatoria1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fconvocatoria1.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/convocatoria2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fconvocatoria2.mdx&astroContentModuleFlag=true")],
@@ -8,5 +9,6 @@ export default new Map([
 ["src/content/blog/El patrimonio no se conserva se vive.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FEl+patrimonio+no+se+conserva+se+vive.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/mito-del-sereno.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmito-del-sereno.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/primer-post.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fprimer-post.mdx&astroContentModuleFlag=true")],
-["src/content/blog/proximos_anuncios.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fproximos_anuncios.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/proximos_anuncios.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fproximos_anuncios.mdx&astroContentModuleFlag=true")],
+["src/content/blog/toque-de-cacho-por-las-trochas-de-la-memoria.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ftoque-de-cacho-por-las-trochas-de-la-memoria.mdx&astroContentModuleFlag=true")]]);
 		
