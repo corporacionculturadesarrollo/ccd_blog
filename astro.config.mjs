514cd7c75533d909bf73b64afb6f18ed4b2a1856
corporacionculturadesarrollo <corporacionculturadesarrollo@gmail.com>
2026-09-21 21:55:44 -0500

    Fix orden cronologico, filtro autor, formulario nativo y limpieza React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://blog.culturaydesarrollo.org',
-  integrations: [mdx(), react()],
+  integrations: [mdx()],
 });
